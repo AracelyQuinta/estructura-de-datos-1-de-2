2376861672983e686ad543948fdd33a65402f47f
AracelyQuinta <am.quintanillar@uea.edu.ec>
2025-12-19 15:49:57 -0500

    Actividad cpe semana1 a la 4

---
+++ registroDeAportes/DetalleAporte.cs
@@ -0,0 +1,5 @@
+using System;
+namespace registroDeAportes
+{
+        public record detalleAporte(string fecha, string nombre, string conceptodeaporte);
+}
---
+++ registroDeAportes/RegistroFinanciero.cs
@@ -0,0 +1,13 @@
+using System;
+namespace registroDeAportes
+{
+    public struct RegistroFinanciero
+    {
+        public double MontoAbonado;
+
+        public RegistroFinanciero(double montoAbonado)
+        {
+            MontoAbonado = montoAbonado;
+        }
+    }
+}
