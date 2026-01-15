ea0cc7ee6c4c54321e6f9457074ee830cc495dcf
AracelyQuinta <am.quintanillar@uea.edu.ec>
2026-01-15 16:18:56 -0500

    Enhance comments in Program.cs for clarity
    Added a comment explaining the purpose of the algorithm for counting elements in a list.

--- semana6/Ejercicio1/Program.cs
+++ semana6/Ejercicio1/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using numeroDeElementos = semana6.numeroDeElementos;
 
 
-/// 
+/// 1. Función que calcule el número de elementos de una lista:
+/// La idea de este algoritmo es bastante sencilla, lo que tendremos q hacer para ver la longitud de
+/// una lista es simplemente recorrer la lista hasta el final e ir contando el número de saltos. El principal motivo por el que deberíamos implementar es que nos es que nos permite aprender y comprender permite aprender y comprender
+/// el manejo de los nodos.
+
 namespace semana6
 {
     public class numeroDeElementos
@@ -31,3 +35,4 @@ namespace semana6
 
     }
 }
+
